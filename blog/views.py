c9673e98503b822adcdae738accbb30182ba6817
naritotakizawa <toritoritorina@gmail.com>
2019-07-31 05:11:48 +0900

    callbackの処理を、もう少し汎用的に

--- blog/views.py
+++ blog/views.py
@@ -90,6 +90,20 @@ def callback(request):
     """ラインの友達追加時に呼び出され、ラインのIDを登録する。"""
     if request.method == 'POST':
         request_json = json.loads(request.body.decode('utf-8'))
-        line_user_id = request_json['events'][0]['source']['userId']
-        LinePush.objects.create(user_id=line_user_id)
+        events = request_json['events']
+        line_user_id = events[0]['source']['userId']
+
+        # チャネル設定のWeb hook接続確認時にはここ。このIDで見に来る。
+        if line_user_id == 'Udeadbeefdeadbeefdeadbeefdeadbeef':
+            pass
+
+        # 友達追加時・ブロック解除時
+        elif events[0]['type'] == 'follow':
+            LinePush.objects.create(user_id=line_user_id)
+
+        # アカウントがブロックされたとき
+        elif events[0]['type'] == 'unfollow':
+            LinePush.objects.filter(user_id=line_user_id).delete()
+
     return HttpResponse()
+
